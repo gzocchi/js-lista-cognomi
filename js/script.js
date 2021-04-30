1942683f619d2ef86f1f5d2a0461743081fd5b96
gzocchi <z.giorgio.z@gmail.com>
2021-04-30 16:56:43 +0200

    modificato condizione while

--- js/script.js
+++ js/script.js
@@ -2,18 +2,15 @@
 // Consegna:
 // 1. chiedi all'utente il cognome
 var cognomeUtente = prompt("Scrivi il tuo cognome");
-
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 cognomi.push(cognomeUtente);
 // debug
 console.log("cognomi prima del sort: ", cognomi);
-
 // 3. stampa la lista ordinata alfabeticamente
 cognomi.sort();
 // debug
 console.log("cognomi dopo il sort: ", cognomi);
-
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 for (i = 0; i < cognomi.length; i++) {
   console.log(i + 1, "-", cognomi[i]);
@@ -24,15 +21,16 @@ var arrayCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
 do {
   var cognomeUtente = prompt("Scrivi il tuo cognome");
-} while (cognomeUtente[0] == " " || cognomeUtente == "" || parseInt(cognomeUtente));
-// ripeto la domanda se la stringa ha:
-// uno spazio iniziale - stringa vuota - un numero iniziale
+  //elimino eventuali spazi iniziali e finali + stringa minuscola
+  cognomeUtente = cognomeUtente.trim().toLowerCase();
+} while ( cognomeUtente == "" || parseInt(cognomeUtente));
+// ripeto la domanda se la stringa è vuota o ha un numero iniziale
 
 // debug
 console.log(cognomeUtente);
 
 // porto il cognome ad avere la prima lettera maiuscola
-var cognome = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
+var cognome = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
 // porto i cognomi composti ad avere ogni prima lettera maiuscola
 for (i = 0; i < cognome.length; i++) {
   if (cognome[i] == " ") {
@@ -49,7 +47,7 @@ arrayCognomi.sort();
 // stampo elenco cognomi
 for (i = 0; i < arrayCognomi.length; i++) {
   console.log(i + 1, "-", arrayCognomi[i]);
-  
+
   if (arrayCognomi[i] == cognome) {
     document.getElementById("cognomi").innerHTML += '<li id="cognome_utente">' + arrayCognomi[i] + "</li>"
   } else {
