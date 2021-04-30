6ea4e5bd94bd7d3a355453f1deea68f830d571e2
gzocchi <z.giorgio.z@gmail.com>
2021-04-30 18:16:34 +0200

    aggiunto commenti

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-/*
 // Consegna:
 // 1. chiedi all'utente il cognome
-var cognomeUtente = prompt("Scrivi il tuo cognome");
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+/*
+var cognomeUtente = prompt("Scrivi il tuo cognome");
 var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 cognomi.push(cognomeUtente);
-// debug
-console.log("cognomi prima del sort: ", cognomi);
-// 3. stampa la lista ordinata alfabeticamente
+console.log("cognomi prima del sort: ", cognomi);   // debug
 cognomi.sort();
-// debug
-console.log("cognomi dopo il sort: ", cognomi);
-// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+console.log("cognomi dopo il sort: ", cognomi);     // debug
 for (i = 0; i < cognomi.length; i++) {
   console.log(i + 1, "-", cognomi[i]);
 }
@@ -21,9 +20,9 @@ var arrayCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
 do {
   var cognomeUtente = prompt("Scrivi il tuo cognome");
+  console.log("Cognome utente:",cognomeUtente); // debug
   //elimino eventuali spazi iniziali e finali + stringa minuscola
   cognomeUtente = cognomeUtente.trim().toLowerCase();
-  
   // cerco eventuali numeri all'interno della stringa
   var presenzaNumeri = false;
   /*var n = 0;
@@ -33,19 +32,14 @@ do {
       };
       n++;
   };*/
-
   for (var n = 0; n < cognomeUtente.length; n++) {
     if (isNaN(parseInt(cognomeUtente[n])) == false) {
         presenzaNumeri = true;
         break;
     };
   };
-  
 } while (cognomeUtente == "" || parseInt(cognomeUtente) || presenzaNumeri);
 // ripeto la domanda se la stringa è vuota, se ha un numero iniziale o contiene un numero
-
-// debug
-console.log(cognomeUtente);
 
 // porto il cognome ad avere la prima lettera maiuscola
 var cognome = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
@@ -58,13 +52,15 @@ for (i = 0; i < cognome.length; i++) {
   };
 };
 
+console.log("Conome utente modificato:",cognome); // debug
+
 // aggiungo il cognome manipolato all'array
 arrayCognomi.push(cognome);
 // ordine alfabetico
 arrayCognomi.sort();
 // stampo elenco cognomi
 for (i = 0; i < arrayCognomi.length; i++) {
-  console.log(i + 1, "-", arrayCognomi[i]);
+  console.log(i + 1, arrayCognomi[i]);
 
   if (arrayCognomi[i] == cognome) {
     document.getElementById("cognomi").innerHTML += '<li id="cognome_utente">' + arrayCognomi[i] + "</li>"
