e0aefa4a3d3cff2ece777ce769610d9fa85a7953
gzocchi <z.giorgio.z@gmail.com>
2021-04-30 13:44:58 +0200

    aggiunto script cognomi

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Consegna:
 // 1. chiedi all'utente il cognome
+var cognomeUtente = prompt("Scrivi il tuo cognome");
+
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
+var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+cognomi.push(cognomeUtente);
+// debug
+console.log("cognomi prima del sort: ", cognomi);
+
 // 3. stampa la lista ordinata alfabeticamente
+cognomi.sort();
+// debug
+console.log("cognomi dopo il sort: ", cognomi);
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+for (i = 0; i < cognomi.length; i++) {
+  console.log(i + 1, "-", cognomi[i]);
+}
